959283d229e83ab8367db502752da18ca026a1a0
fred374 <fred.kelemen99@gmail.com>
2024-05-15 12:53:59 -0700

    Question 5 - Created new spell, added it to the spell list, and made new effect area for it

---
+++ forgottenserver/data/spells/scripts/attack/winter.lua
@@ -0,0 +1,19 @@
+--A copy of the eternal winter spell which is decidedly similar to the effect we want.
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+
+-- A smaller are of effect than the eternal winter variant.
+combat:setArea(createCombatArea(AREA_CIRCLE5X5_ALT))
+
+function onGetFormulaValues(player, level, magicLevel)
+	local min = (level / 5) + (magicLevel * 5.5) + 25
+	local max = (level / 5) + (magicLevel * 11) + 50
+	return -min, -max
+end
+
+combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
+function onCastSpell(creature, variant)
+	return combat:execute(creature, variant)
+end
